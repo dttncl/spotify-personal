6a16bc1b799ecd221f6329c4594f9ba158fa2230
dttncl <82695034+dttncl@users.noreply.github.com>
2024-02-27 09:43:57 -0500

    added authorization

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { SpotifyService } from '../spotify.service';
+import { User } from '../user';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-banner',
@@ -10,9 +12,17 @@ import { SpotifyService } from '../spotify.service';
 })
 export class BannerComponent {
 
+  user$: Observable<User | null> | undefined;
+  userDisplay : User | null | undefined;
+  
   constructor(private spotifyService: SpotifyService) {}
-
-  search(user_id: string): void {
-    this.spotifyService.generatePlaylists(user_id);
+  
+  
+  ngOnInit() {
+    this.user$ = this.spotifyService.user;
+    this.user$.subscribe(result => this.userDisplay = result);
+    console.log(this.userDisplay);
   }
+  
+
 }
--- src/app/spotify.service.ts
+++ src/app/spotify.service.ts
@@ -1,37 +1,119 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs';
-
-
+import { BehaviorSubject, Observable, map } from 'rxjs';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class SpotifyService {
 
-  constructor(private http: HttpClient) { }
+  private clientId = "2975874d026e40f48153f609602a8578";
+  private params = new URLSearchParams(window.location.search);
+  private code = this.params.get("code");
 
-  private url = "https://api.spotify.com/v1/users/";
-  private keyword = "/playlists?offset=0&limit=20";
+  private _user: BehaviorSubject<User | null> = new BehaviorSubject<User | null>(null);
+  public readonly user: Observable<User | null> = this._user.asObservable();
 
-  createEndpoint(user_id: string) {
-    // generate ENDPOINT
-    console.log(this.url + user_id + this.keyword);
-    return this.url + user_id + this.keyword;
+  constructor(private http: HttpClient) { 
+    if (!this.code) {
+      this.redirectToAuthCodeFlow(this.clientId);
+    } else {
+      this.initialize();
+    }
   }
 
-  /*
-  generatePlaylists(user_id: string) : void {
-    let endpoint = this.createEndpoint(user_id);
+  public getUserValue(): User | null {
+    return this._user.getValue();
   }
-  */
+  
+  private initialize() {
+    const ofAccessToken = this.getAccessToken(this.clientId, this.code!);
+    ofAccessToken.subscribe((accessToken: string) => {
+      this.generateUser(accessToken).subscribe((user: User) => {
+        this._user.next(user); // update the user variable
+        console.log(user)});
+    });
+  }
+  
+  private async redirectToAuthCodeFlow(clientId: string) {
+    const verifier = this.generateCodeVerifier(128);
+    const challenge = await this.generateCodeChallenge(verifier);
 
-  generatePlaylists(user_id: string) {
-    let endpoint = this.createEndpoint(user_id);
+    localStorage.setItem("verifier", verifier);
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("response_type", "code");
+    params.append("redirect_uri", "http://localhost:4200/");
+    params.append("scope", "user-read-private user-read-email");
+    params.append("code_challenge_method", "S256");
+    params.append("code_challenge", challenge);
+
+    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+  }
+  
+  private generateCodeVerifier(length: number) {
+      let text = '';
+      let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+      for (let i = 0; i < length; i++) {
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+      return text;
+  }
+  
+  private async generateCodeChallenge(codeVerifier: string) {
+      const data = new TextEncoder().encode(codeVerifier);
+      const digest = await window.crypto.subtle.digest('SHA-256', data);
+      return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+          .replace(/\+/g, '-')
+          .replace(/\//g, '_')
+          .replace(/=+$/, '');
+  }
+
+  private getAccessToken(clientId: string, code: string) : Observable<string> {
+    const verifier = localStorage.getItem("verifier");
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("grant_type", "authorization_code");
+    params.append("code", code);
+    params.append("redirect_uri", "http://localhost:4200/");
+    params.append("code_verifier", verifier!);
+
     const headers = new HttpHeaders()
-      .set('Authorization','Bearer BQAEtOfVwQs6GynHxlUrMKKujs5XqVryNjo07gauLiOJijaNoYIJSSjikMpG1XfpF7hZFBvvO7D-70CU4UrLkVQc7n45bfGiXPmm41-qOrIMMkeqtqA');
-    return this.http.get(endpoint, {headers}).pipe(map(data => {})).subscribe(result => {
-      console.log(result);
+                        .set('Content-Type','application/x-www-form-urlencoded');
+
+    return this.http.post('https://accounts.spotify.com/api/token', params.toString(), { headers: headers })
+    .pipe(map((response: any) => {
+      return response.access_token;
+    }));
+  }
+
+  generateUser(token: string): Observable<User> {
+    return new Observable<User>((observer) => {
+        let endpoint = 'https://api.spotify.com/v1/me';
+
+        const headers = new HttpHeaders()
+            .set('Authorization', `Bearer ${token}`);
+
+        this.http.get(endpoint, { headers }).subscribe(result => {
+            let idResult = Object.values(result)[3];
+            let nameResult = Object.values(result)[0];
+
+            let user: User = {
+                id: idResult,
+                name: nameResult
+            };
+
+            observer.next(user); // emit the user object
+            observer.complete(); // complete the observable
+        }, error => {
+            console.error('HTTP Error', error);
+            observer.error(error); 
+        });
     });
   }
 
